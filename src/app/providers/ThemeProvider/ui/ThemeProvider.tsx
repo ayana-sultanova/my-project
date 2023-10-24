5be0bcb260acd0ba57abcd8008643fb58ac93e21
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2023-10-24 15:15:15 +0600

    add babel, add extract plugin

---
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -0,0 +1,19 @@
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext, ThemeContextProps} from "../lib/ThemeContext";
+import {FC, useMemo, useState} from "react";
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) || Theme.LIGHT
+const ThemeProvider:FC = ({children}) => {
+    const [theme, setTheme] = useState(defaultTheme)
+
+    const defaultProps = useMemo((): ThemeContextProps => ({
+        theme: theme,
+        setTheme: setTheme
+    }), [theme])
+    return (
+        <ThemeContext.Provider value={defaultProps}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeProvider
