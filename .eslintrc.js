2d6a49e7d8064f153c5a3a4d317785b7f18db023
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2023-11-03 13:58:15 +0600

    fixed dev plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,55 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "standard-with-typescript",
-        "plugin:react/recommended",
-        "plugin:i18next/recommended",
-        "plugin:storybook/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "i18next"
-    ],
-    "rules": {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/prefer-nullish-coalescing': 'off',
-        '@typescript-eslint/strict-boolean-expressions': 'off',
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/no-floating-promises': 'off',
-        '@typescript-eslint/naming-convention': 'off',
-        'react/no-deprecated': 'off',
-        'i18next/no-literal-string': [
-            'error',
-            {
-                markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to']
-            }
-        ]
-    },
-    globals: {
-        __IS_DEV__: true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:i18next/recommended',
+    'plugin:storybook/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'i18next'
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    'react/no-deprecated': 'off',
+    'i18next/no-literal-string': [
+      'error',
+      {
+        markupOnly: true,
+        ignoreAttribute: ['data-testid', 'to']
+      }
+    ]
+  },
+  globals: {
+    __IS_DEV__: true
+  }
 }
--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,12 +1,12 @@
-import {ReactNode} from "react";
-import i18nForTests from "../../../config/i18n/i18nForTests";
-import {I18nextProvider} from "react-i18next";
-import {render} from "@testing-library/react";
+import { type ReactNode } from 'react'
+import i18nForTests from '../../../config/i18n/i18nForTests'
+import { I18nextProvider } from 'react-i18next'
+import { render } from '@testing-library/react'
 
-export function renderWithTranslation(component: ReactNode) {
-    return render(
+export function renderWithTranslation (component: ReactNode) {
+  return render(
         <I18nextProvider i18n={i18nForTests}>
             {component}
         </I18nextProvider>
-    )
+  )
 }
