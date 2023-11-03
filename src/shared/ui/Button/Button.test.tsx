8f273aa9b345a9c04fbd3fa8bfa42a356555e427
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2023-11-03 15:34:29 +0600

    fixed linter

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -12,6 +12,5 @@ describe('button', () => {
     // eslint-disable-next-line i18next/no-literal-string
     render(<Button theme={ThemeButton.CLEAR}>TEST</Button>)
     expect(screen.getByText('TEST')).toHaveClass('clear')
-    screen.debug()
   })
 })
