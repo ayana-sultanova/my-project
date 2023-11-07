8889e8a93696303a108c578ad7cefb07a398c035
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2023-11-07 20:38:49 +0600

    fix tests

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,22 @@
+import { type ReactNode } from 'react'
+import i18nForTests from '../../../config/i18n/i18nForTests'
+import { I18nextProvider } from 'react-i18next'
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+export interface componentRenderProps {
+  route?: string
+}
+
+export function componentRender (component: ReactNode, options: componentRenderProps = {}) {
+  const {
+    route = '/'
+  } = options
+  return render(
+        <MemoryRouter>
+            <I18nextProvider i18n={i18nForTests}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>
+  )
+}
