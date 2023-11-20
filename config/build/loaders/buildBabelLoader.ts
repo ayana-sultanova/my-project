1ebb1e403ca14e90d7fa6b6e21045a21c8e46fae
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2023-11-21 01:43:13 +0600

    add new theme, add react refresh plugin

---
+++ config/build/loaders/buildBabelLoader.ts
@@ -0,0 +1,27 @@
+import { type BuildOptions } from '../types/config'
+
+export function buildBabelLoader (options: BuildOptions) {
+  return {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', { targets: 'defaults' }]
+        ],
+        plugins: [
+          ['i18next-extract',
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true
+            }
+          ],
+          options.isDev && require.resolve('react-refresh/babel')
+        ].filter(
+          Boolean
+        )
+      }
+    }
+  }
+}
