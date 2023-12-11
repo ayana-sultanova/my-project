b5e92013b2ba2761ae262284e9802102cc379507
ayana-sultanova <aiana.sultanovaa@gmail.com>
2023-12-11 17:26:03 +0600

    add filters, search

---
+++ src/features/scrollSave/index.ts
@@ -0,0 +1,3 @@
+export type { UISchema } from './model/types/UISchema'
+export { getUIScrollByPath } from './model/selectors/getUIScroll'
+export { scrollSaveReducer } from './model/slices/scrollSaveSlice'
